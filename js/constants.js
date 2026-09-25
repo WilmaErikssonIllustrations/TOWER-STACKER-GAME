fd51e91e0efdb582f5c1464d0c07771e9db0dcf2
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-09-25 14:07:52 +0200

    feat: add logo images and update constants

--- js/constants.js
+++ js/constants.js
@@ -7,3 +7,19 @@ export const GAME_OVER_FALL_DISTANCE = 200;
 export const GAME_OVER_DELAY = 1200;
 export const CONTAINER_PADDING = 20;
 export const SPAWN_TOP_POSITION = "10px";
+export const LOGOS = [
+  "/assets/angbyiflogo.jpeg",
+  "assets/be-active-day-logo.png",
+  "assets/bjorklinge-traningscenter-logo.png",
+  "assets/formgotlandlogo.png",
+  "assets/friskislogo.png",
+  "assets/hagabadet-logo.jpeg",
+  "assets/jjlogo.jpeg",
+  "assets/kristinedalslogo.jpg",
+  "assets/malkarslogo.jpg",
+  "assets/satslogo.png",
+  "assets/sgylogo.jpg",
+  "assets/stclogo.png",
+  "assets/team-isak-logo.png",
+  "assets/umlogo.jpeg",
+];
